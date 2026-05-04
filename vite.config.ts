e2e913e27b8c2f76ec29a3b8b5a5df26978bcca7
khangnguyenspu <nguyentrantuankhang0103@gmail.com>
2026-05-04 15:16:58 -0700

    fix github publish page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/khang-nguyen-portfolio/',
+  base: mode === "production" ? "/khang-nguyen-portfolio/" : "/",
   server: {
     host: "::",
     port: 8080,
